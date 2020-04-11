0ae986eff3ed4fe3d2f80cc0000b68cb1dcc3bba
zhangyu-git <641346589@qq.com>
2020-04-12 06:58:20 +0800

    05-Insertion-Sort

---
+++ 02-Sorting-Basic/05-Insertion-Sort/SelectionSort.h
@@ -0,0 +1,24 @@
+#ifndef __SELECTIONSORT_H
+#define __SELECTIONSORT_H
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+template<typename T>
+void selectionSort(T arr[], int n){
+
+    for(int i = 0 ; i < n ; i ++){
+
+        int minIndex = i;
+        for( int j = i + 1 ; j < n ; j ++ )
+            if( arr[j] < arr[minIndex] )
+                minIndex = j;
+
+        swap( arr[i] , arr[minIndex] );
+    }
+}
+
+#endif //__SELECTIONSORT_H
